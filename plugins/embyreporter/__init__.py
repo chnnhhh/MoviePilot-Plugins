13f650d3b0b47df68bba285a54d4322509017e74
jiejian.liu <jiejian.liu@amh-group.com>
2024-07-11 15:21:53 +0800

    fix bug

--- plugins/embyreporter/__init__.py
+++ plugins/embyreporter/__init__.py
@@ -34,7 +34,7 @@ class EmbyReporter(_PluginBase):
     # 插件图标
     plugin_icon = "Pydiocells_A.png"
     # 插件版本
-    plugin_version = "1.9"
+    plugin_version = "2.0"
     # 插件作者
     plugin_author = "chnnhhh"
     # 作者主页
